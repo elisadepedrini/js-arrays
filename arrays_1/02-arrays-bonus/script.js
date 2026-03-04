77ca195810c34f5d01152acb05af8fc20bd9390c
elisadepedrini <elisa.depedrini@gmail.com>
2026-03-04 18:45:49 +0100

    Complete bonus's task 3

--- arrays_1/02-arrays-bonus/script.js
+++ arrays_1/02-arrays-bonus/script.js
@@ -19,9 +19,21 @@ for (let i = teachers.length - 1; i >= 0; i--) {
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+
+// for (let i = 0; i< teachers.length; i++) {
+
+//   if (teachers[i].length >= 5) {
+//     const longNames = teachers.slice();
+//     console.log(longNames);
+// }
+  
+// }
 
 // 3. Rimuovi 'Ed' dall'array teachers
+
+teachers.splice(1, 1)
+console.log(teachers);
+
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
